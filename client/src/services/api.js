cf66810a159ad336ab4d4f6d99a3b9580f1cbe1e
Ravi Kumar <04ravi.rk@gmail.com>
2025-03-14 00:31:35 +0530

    url update2.0

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for API requests
-const API_BASE_URL = 'https://couponx.onrender.com';
+const API_BASE_URL = 'https://couponx.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
